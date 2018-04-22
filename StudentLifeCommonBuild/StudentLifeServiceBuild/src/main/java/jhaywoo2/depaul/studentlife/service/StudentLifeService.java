71f90a00b8dadd12ae1d57cd74b71fe4ec25bf6c
finalfantasyguru8705 <finalfantasyguru8705@gmail.com>
2018-04-21 20:05:20 -0500

    fixed autowiring issue..i think

--- StudentLifeCommonBuild/StudentLifeServiceBuild/src/main/java/jhaywoo2/depaul/studentlife/service/StudentLifeService.java
+++ StudentLifeCommonBuild/StudentLifeServiceBuild/src/main/java/jhaywoo2/depaul/studentlife/service/StudentLifeService.java
@@ -3,8 +3,6 @@ package jhaywoo2.depaul.studentlife.service;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-@Service("studentLifeService")
 public interface StudentLifeService {
    public List getAllStudents();
 }
--- StudentLifeCommonBuild/StudentLifeServiceBuild/src/main/java/jhaywoo2/depaul/studentlife/serviceimpl/StudentLifeServiceImpl.java
+++ StudentLifeCommonBuild/StudentLifeServiceBuild/src/main/java/jhaywoo2/depaul/studentlife/serviceimpl/StudentLifeServiceImpl.java
@@ -3,10 +3,13 @@ package jhaywoo2.depaul.studentlife.serviceimpl;
 import jhaywoo2.depaul.studentlife.service.StudentLifeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import jhaywoo2.depaul.studentlife.repository.StudentRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("studentServiceLife")
 public class StudentLifeServiceImpl implements StudentLifeService {
+
     @Autowired
     protected StudentRepository studentRepository;
 
