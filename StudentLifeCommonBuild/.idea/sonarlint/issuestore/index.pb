1b68a9836090499a677dc8ce23f99d0afc0c8606
finalfantasyguru8705 <finalfantasyguru8705@gmail.com>
2018-06-26 20:57:32 -0500

    bf

--- StudentLifeCommonBuild/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,7 +0,0 @@
-
-7
-pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
-O
-StudentLifeServiceBuild/pom.xml,f/c/fc6ef0570b1fa35ba0b9f35b560db14771ae401d
-K
-StudentLifeWebBuild/pom.xml,c/0/c0e1a2cf880939cce1c24f444bc57ea638a93731
---
+++ StudentLifeWebBuild/src/main/java/jhaywoo2/depaul/studentlife/config/DatasourceConfiguration.java
@@ -0,0 +1,84 @@
+package jhaywoo2.depaul.studentlife.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.net.URISyntaxException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "jhaywoo2.depaul.studentlife")
+@EnableJpaRepositories(basePackages = "jhaywoo2.depaul.studentlife.repository")
+@EnableTransactionManagement
+@EnableJpaAuditing
+public class DatasourceConfiguration {
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws URISyntaxException {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+
+		em.setPackagesToScan("jhaywoo2.depaul.studentlife.model");
+
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(databaseProperties());
+		em.setDataSource(localDataSource());
+		return em;
+	}
+
+	@Bean
+	public DataSource localDataSource() {
+		JndiDataSourceLookup jndiLookUp = new JndiDataSourceLookup();
+		jndiLookUp.setResourceRef(true);
+		return jndiLookUp.getDataSource(databaseProperties().getProperty("jndiName"));
+	}
+	// @Bean
+	// public BasicDataSource dataSource() throws URISyntaxException {
+	// String dbUrl = System.getenv("JDBC_DATABASE_URL");
+	// String username = System.getenv("JDBC_DATABASE_USERNAME");
+	// String password = System.getenv("JDBC_DATABASE_PASSWORD");
+	//
+	// BasicDataSource basicDataSource = new BasicDataSource();
+	// basicDataSource.setUrl(dbUrl);
+	// basicDataSource.setMessageRecipient(username);
+	// basicDataSource.setPassword(password);
+	//
+	// return basicDataSource;
+	// }
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	public Properties databaseProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("jndiName", "java:/jdbc/StudentLife");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		properties.setProperty("spring.datasource.testWhileIdle", "true");
+		properties.setProperty("c3p0.preferredTestQuery", "SELECT 1");
+		properties.setProperty("c3p0.maxIdleTime", "10");
+		properties.setProperty("spring.datasource.timeBetweenEvictionRunsMillis", "63600000");
+		properties.setProperty("c3p0.testConnectionOnCheckout", "true");
+		return properties;
+	}
+}
